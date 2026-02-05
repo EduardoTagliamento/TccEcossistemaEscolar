97ec012a2b9578c98e7383c1b5a6a856075c138b
Eduardo Tagliamento Barbosa <ti.eduardotagliamento@gmail.com>
2026-02-05 16:26:59 -0300

    chave

--- backend/utils/external/sendBrevoEmail.js
+++ backend/utils/external/sendBrevoEmail.js
@@ -8,7 +8,7 @@ const axios = require('axios');
  * @returns {Promise<object>} - Resposta da API Brevo
  */
 async function sendBrevoEmail(toEmail) {
-  const apiKey = 'xsmtpsib-a26634b12a93b07082479c5eeb3eca68d4bf6e2ecd4ea25e164b37a078089d64-x5jX8ffbCHHEszuk'; // Substitua pela sua chave
+  const apiKey = 'CHAVE'; // Substitua pela sua chave
   const url = 'https://api.brevo.com/v3/smtp/email';
 
   const data = {
