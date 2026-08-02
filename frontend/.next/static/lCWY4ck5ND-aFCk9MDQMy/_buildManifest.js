3bf6968cce3b7261002f588720acdbdc131178bd
EduardoTagliamento <ti.eduardotagliamento@gmail.com>
2026-08-02 18:46:42 -0300

    bibliotecas

---
+++ frontend/.next/static/lCWY4ck5ND-aFCk9MDQMy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:e,source:"/api/:path*",destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/framework-1cf2d8a0fde7c934.js","static/chunks/pages/_error-6ad85a9b843cdf24.js"],sortedPages:["/_app","/_error"]}}(void 0),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
